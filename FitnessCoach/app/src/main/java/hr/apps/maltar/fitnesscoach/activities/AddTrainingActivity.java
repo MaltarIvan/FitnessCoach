958499ed10811a42f64e2e5f6acf9812777befb7
MaltarIvan <ivan.maltar@fer.hr>
2018-03-20 23:21:25 +0100

    cancel button in add training activity now return to day activity

--- FitnessCoach/app/src/main/java/hr/apps/maltar/fitnesscoach/activities/AddTrainingActivity.java
+++ FitnessCoach/app/src/main/java/hr/apps/maltar/fitnesscoach/activities/AddTrainingActivity.java
@@ -58,7 +58,7 @@ public class AddTrainingActivity extends AppCompatActivity {
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                finish();
             }
         });
 
